5c3e0a96625372a3ecf22401b88a0c06c2f9447d
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 15:53:19 +0100

    Add interface IContact

---
+++ Shared/Interfaces/IContact.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Interfaces
+{
+    public interface IContact
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
+        string Address { get; set; }
+    }
+}
