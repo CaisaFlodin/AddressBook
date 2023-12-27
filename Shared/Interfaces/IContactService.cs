a0dac46536810c19063c2f18bb5572af6f4fb2e2
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 15:59:59 +0100

    Create interface IContactService

---
+++ Shared/Interfaces/IContactService.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.Interfaces;
+
+public interface IContactService
+{
+    bool AddContactToList(IContact contact);
+
+    IEnumerable<IContact> GetAllContactsFromList();
+
+    IContact GetContactFromList(string email);
+
+    bool UpdateContactInList(string email, IContact updatedContact);
+
+    bool RemoveContactFromList(string email);
+}
