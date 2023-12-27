7cb903b47c589436e712ce064c6fdde96026e5cb
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 15:57:10 +0100

    Implement interface in Contact model

--- Shared/Models/Contact.cs
+++ Shared/Models/Contact.cs
@@ -1,5 +1,12 @@
-﻿namespace Shared.Models;
+﻿using Shared.Interfaces;
 
-public class Contact
+namespace Shared.Models;
+
+public class Contact() : IContact
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Address { get; set; } = null!;
 }
