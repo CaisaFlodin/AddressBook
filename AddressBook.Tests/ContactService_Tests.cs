10e33795702dd8a5a8ccd2937f31c508e242c0e9
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 16:05:50 +0100

    Create test for adding contact to list

--- AddressBook.Tests/ContactService_Tests.cs
+++ AddressBook.Tests/ContactService_Tests.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using Shared.Interfaces;
+using Shared.Models;
+using Shared.Services;
 
-namespace AddressBook.Tests
+namespace AddressBook.Tests;
+
+public class ContactService_Tests
 {
-    internal class ContactService_Tests
+    [Fact]
+    public void AddContactToListShould_AddOneContactToContactList_ThenReturnTrue()
     {
+        // Arrange
+        IContact contact = new Contact { FirstName = "Test", LastName = "Testsson", PhoneNumber = "0733477354", Email = "test@domain.com", Address = "Testvägen 6" };
+
+        var mockFileService = new Mock<IFileService>();
+        IContactService contactService = new ContactService(mockFileService.Object);
+
+
+        // Act
+        bool result = contactService.AddContactToList(contact);
+
+
+        // Assert
+        Assert.True(result);
     }
 }
