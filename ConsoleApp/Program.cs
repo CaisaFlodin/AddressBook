4c72a9b6632e0e6f017910d2be930d1e95e0719d
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 16:38:50 +0100

    Implement dependency injection

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ConsoleApp
+﻿using ConsoleApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shared.Interfaces;
+using Shared.Services;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<IFileService, FileService>();
+                services.AddSingleton<IContactService, ContactService>();
+                services.AddSingleton<MenuService>();
+                //services.AddSingleton<IMenuService, MenuService>();
+            }).Build();
+
+            builder.Start();
+            Console.Clear();
+
+            var menuService = builder.Services.GetRequiredService<MenuService>();
+            menuService.StartMenu();
         }
     }
 }
