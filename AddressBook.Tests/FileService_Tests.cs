b95bc77fb1ca4234eaed86e6cef65aed56dbcef8
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 15:43:59 +0100

    Add test for save to file

---
+++ AddressBook.Tests/FileService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Interfaces;
+using Shared.Services;
+
+namespace AddressBook.Tests;
+
+public class FileService_Tests
+{
+    [Fact]
+    public void SaveToFileShould_ReturnTrue_IfFilePathExists()
+    {
+        // Arrange
+        IFileService fileService = new FileService();
+        string filePath = @"c:\Projects\test.text";
+        string content = "Test content";
+
+        // Act
+        bool result = fileService.SaveContentToFile(filePath, content);
+
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ Shared/Services/FileService.cs
@@ -0,0 +1,35 @@
+﻿using Shared.Interfaces;
+using System.Diagnostics;
+
+namespace Shared.Services;
+
+public class FileService : IFileService
+{
+    public bool SaveContentToFile(string filePath, string content)
+    {
+        try
+        {
+            using (var sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine(content);
+                return true;
+            }
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return false;
+    }
+
+    public string GetContentFromFile(string filePath)
+    {
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                using var sr = new StreamReader(filePath);
+                return sr.ReadToEnd();
+            }
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+    }
+}
