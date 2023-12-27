89ca5262f043f1a6413417618a4854e6d41fa6da
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-27 16:27:25 +0100

    Create interface IMenuService

---
+++ ConsoleApp/Interfaces/IMenuService.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Interfaces;
+
+namespace ConsoleApp.Interfaces;
+
+public interface IMenuService
+{
+    void StartMenu();
+    void ShowAddContactOption();
+    void ShowContactsOption();
+    void ShowContactInfoOption();
+    void ShowDeleteContactOption();
+    void ShowUpdateContactOption();
+    IContact GetUpdatedContact();
+}
