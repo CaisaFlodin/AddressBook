a34ea1553ba36423057014a27a1869d8986f7eb6
Caisa Flodin <caisaflodin@hotmail.com>
2023-12-28 11:42:12 +0100

    Add views

---
+++ MobileApp/Views/AddView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Views;
+
+public partial class AddView : ContentPage
+{
+	public AddView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MobileApp/Views/EditView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Views;
+
+public partial class EditView : ContentPage
+{
+	public EditView()
+	{
+		InitializeComponent();
+	}
+}
